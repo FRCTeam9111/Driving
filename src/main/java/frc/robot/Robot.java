a6fbb4a8d5ad23696df6111b91b3933be2affdc2
msMcWilliams <kristi_mcwilliams@crpusd.org>
2024-02-14 17:47:11 -0800

    arcade drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,7 +4,13 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 
 /**
@@ -15,14 +21,29 @@ import edu.wpi.first.wpilibj.TimedRobot;
  */
 public class Robot extends TimedRobot {
  
-
+  private final CANSparkMax m_leadMotorleft = new CANSparkMax(3, MotorType.kBrushed);
+  //private final CANSparkMax m_arm = new CANSparkMax(9, MotorType.kBrushless);
+  private final CANSparkMax m_followMotorleft = new CANSparkMax(4, MotorType.kBrushed);
+  private final CANSparkMax m_leadMotorright = new CANSparkMax(2, MotorType.kBrushed);
+  private final CANSparkMax m_followMotorright = new CANSparkMax(1, MotorType.kBrushed);
+  //private final CANSparkMax m_liftMotor = new CANSparkMax(6, MotorType.kBrushless);
+  private final DifferentialDrive m_robotDrive = new DifferentialDrive(m_leadMotorleft, m_leadMotorright);
+  //Joystick controller
+  private final Joystick m_controller = new Joystick(0);
+  //logitech controller
+  private final XboxController xbox_controller = new XboxController(0);
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-  
+   // Syncs the left side of the motors with one another
+   m_followMotorleft.follow(m_leadMotorleft);
+
+   // Syncs the right side of the motors with one another
+   m_followMotorright.follow(m_leadMotorright);
+
   }
 
   /**
@@ -61,7 +82,13 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    //XBox Controller
+    m_robotDrive.arcadeDrive(-xbox_controller.getLeftX() * .8, -xbox_controller.getLeftY() * .8);
+    // joystick 
+    m_robotDrive.arcadeDrive(-m_controller.getRawAxis(0) * .8, -m_controller.getRawAxis(1) * .8);
+   
+  }
 
   @Override
   public void testInit() {
